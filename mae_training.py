1dac287591fd460bbeabbb64eee292373c6d89a6
exx8 <exx8eran@gmail.com>
2023-08-06 12:05:28 +0300

    .

--- mae_training.py
+++ mae_training.py
@@ -52,7 +52,7 @@ def main(args):
     )
     logging.info(f"Dataset size: {len(dataset)}")
     dataloader = utils.data.DataLoader(
-        dataset, batch_size=128, shuffle=True, num_workers=4
+        dataset, batch_size=64, shuffle=True, num_workers=4
     )
     optimizer = optim.Adam(model.parameters(), lr=args.learning_rate)
     scheduler=torch.optim.lr_scheduler.LinearLR(optimizer)
