cb9ee5d3a7184e39941f9f0886b56b81e9ce89d5
exx8 <exx8eran@gmail.com>
2023-08-12 19:54:52 +0300

    .

--- mae_training.py
+++ mae_training.py
@@ -70,7 +70,7 @@ def main(args):
     scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=args.learning_rate, steps_per_epoch=num_batches, epochs=total_epochs)
     ACCUMULATION_STEPS=4
     for epoch in range(40):
-        for batch in tqdm(dataloader_train):
+        for batch_idx,batch in enumerate(tqdm(dataloader_train)):
             batch = batch.to(device)
             loss = model.forward(batch)[0]
             loss/=ACCUMULATION_STEPS
