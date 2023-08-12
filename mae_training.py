8da221b03b56aa25acc1387f801a8f26590ce404
exx8 <exx8eran@gmail.com>
2023-08-12 19:53:37 +0300

    .

--- mae_training.py
+++ mae_training.py
@@ -68,46 +68,19 @@ def main(args):
 
     # Combine both schedulers using SequentialLR
     scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=args.learning_rate, steps_per_epoch=num_batches, epochs=total_epochs)
-    ACCUMULATE_STEP = 4
+    ACCUMULATION_STEPS=4
     for epoch in range(40):
-        accumulated_loss = 0.0  # Initialize accumulated loss for this epoch
-
-        for batch_idx, batch in enumerate(tqdm(dataloader_train)):
-            print(batch_idx)
+        for batch in tqdm(dataloader_train):
             batch = batch.to(device)
             loss = model.forward(batch)[0]
-
-            # Accumulate the loss
-            accumulated_loss += loss
-            del loss
-            del batch
-            if (batch_idx + 1) % ACCUMULATE_STEP == 0:
-                # Backpropagate and update only after accumulating gradients for a certain number of minibatches
-                accumulated_loss /= ACCUMULATE_STEP
+            loss/=ACCUMULATION_STEPS
+            loss.backward()
+            if ((batch_idx + 1) % accum_iter == 0) or (batch_idx + 1 == len(data_loader)):
                 optimizer.zero_grad()
-                accumulated_loss.backward()
                 optimizer.step()
                 scheduler.step()
-
                 if args.log_wandb:
-                    wandb.log({"loss": accumulated_loss, "learning_rate": scheduler.get_last_lr()[0]})
-
-                accumulated_loss = 0.0  # Reset accumulated loss
-
-        # Handle remaining accumulated gradients at the end of the epoch
-        if (batch_idx + 1) % ACCUMULATE_STEP != 0:
-            accumulated_loss /= ((batch_idx + 1) % ACCUMULATE_STEP)
-            optimizer.zero_grad()
-            accumulated_loss.backward()
-            optimizer.step()
-            scheduler.step()
-
-            if args.log_wandb:
-                wandb.log({"loss": accumulated_loss, "learning_rate": scheduler.get_last_lr()[0]})
-        #free batch
-        del batch
-        torch.cuda.empty_cache()
-
+                    wandb.log({"loss": loss, "learning_rate": scheduler.get_last_lr()[0]})
 
     model.eval()
     loss_avg = torch.zeros(1)
